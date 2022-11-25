65812685f7d3c5c394a49ef532ad7fd9aadc684b
fadezilla <harleyseierstad@hotmail.com>
2022-11-25 13:57:11 +0100

    reception dashboard functional with in and out button!

--- Web Application/wdt_app.js
+++ Web Application/wdt_app.js
@@ -74,50 +74,110 @@ fifthTableFill.StaffUserGet()
 $(document).on("click", "#tableBody tr", function(){
     $(this).toggleClass("active bg-success").siblings().removeClass("active bg-success")
 })
-function addHours(numOfHours, date = new Date()) {
-        date.setTime(date.getTime() + numOfHours * 60 * 60 * 1000);
+function addHours(numOfMinutes, date = new Date()) {
+        date.setTime(date.getTime() + numOfMinutes * 60 * 1000);
   
         return date;
     }
-function staffOut(){
-    let status = "out"
-    let duration = (prompt("how long was the absence?"))
-    let time = new Date();
-    let outTime = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds()
-    let addedTime = addHours(duration)
-    let test = new Date(addedTime)
-    let expectedReturnTime = test.getHours() + ":" + test.getMinutes(); + ":" + test.getSeconds();
 
+
+    function staffOut(){
+    if($('#tableBody > tr').hasClass('active') === false){
+        alert("please select a row first")
+       } else{
+        var duration = (prompt("How long is the absence expected to be?"));
+       } 
+
+    var status = "Out"
+    var time = new Date();
+    var outTime = ((time.getHours() < 10)?"0":"") + time.getHours() + ":" + ((time.getMinutes() < 10)?"0":"") + time.getMinutes();
+    var addedTime = addHours(duration);
+    var expectedReturnTime = ((addedTime.getHours() < 10)?"0":"") + addedTime.getHours() + ":" + ((addedTime.getMinutes() < 10)?"0":"") + addedTime.getMinutes()
    if($('#tableBody > tr:nth-child(1)').hasClass('active') === true){
     if(!isNaN(duration)) {
-        $("#tableBody > tr:nth-child(1) > td:nth-child(5)").append(status);
+        $("#tableBody > tr:nth-child(1) > td:nth-child(5)").empty().append(status);
         $("#tableBody > tr:nth-child(1) > td:nth-child(6)").append(outTime);
-        $("#tableBody > tr:nth-child(1) > td:nth-child(7)").append(duration);
+        $("#tableBody > tr:nth-child(1) > td:nth-child(7)").append(duration + ":" + "00");
         $("#tableBody > tr:nth-child(1) > td:nth-child(8)").append(expectedReturnTime);
-        console.log("success!")
         } else {
             alert("invalid input, try again.")
         }
    }
    if($('#tableBody > tr:nth-child(2)').hasClass('active') === true){
-    alert("second")
+    if(!isNaN(duration)) {
+        $("#tableBody > tr:nth-child(2) > td:nth-child(5)").empty().append(status);
+        $("#tableBody > tr:nth-child(2) > td:nth-child(6)").append(outTime);
+        $("#tableBody > tr:nth-child(2) > td:nth-child(7)").append(duration + ":" + "00");
+        $("#tableBody > tr:nth-child(2) > td:nth-child(8)").append(expectedReturnTime);
+        } else {
+            alert("invalid input, try again.")
+        }
    }
    if($('#tableBody > tr:nth-child(3)').hasClass('active') === true){
-    alert("third")
+    if(!isNaN(duration)) {
+        $("#tableBody > tr:nth-child(3) > td:nth-child(5)").empty().append(status);
+        $("#tableBody > tr:nth-child(3) > td:nth-child(6)").append(outTime);
+        $("#tableBody > tr:nth-child(3) > td:nth-child(7)").append(duration + ":" + "00");
+        $("#tableBody > tr:nth-child(3) > td:nth-child(8)").append(expectedReturnTime);
+        } else {
+            alert("invalid input, try again.")
+        }
    }
-   if($('#tableBody > tr:nth-child(2)').hasClass('active') === true){
-    alert("fourth")
+   if($('#tableBody > tr:nth-child(4)').hasClass('active') === true){
+    if(!isNaN(duration)) {
+        $("#tableBody > tr:nth-child(4) > td:nth-child(5)").empty().append(status);
+        $("#tableBody > tr:nth-child(4) > td:nth-child(6)").append(outTime);
+        $("#tableBody > tr:nth-child(4) > td:nth-child(7)").append(duration + ":" + "00");
+        $("#tableBody > tr:nth-child(4) > td:nth-child(8)").append(expectedReturnTime);
+        } else {
+            alert("invalid input, try again.")
+        }
    }
-   if($('#tableBody > tr:nth-child(2)').hasClass('active') === true){
-    alert("fifth")
+   if($('#tableBody > tr:nth-child(5)').hasClass('active') === true){
+    if(!isNaN(duration)) {
+        $("#tableBody > tr:nth-child(5) > td:nth-child(5)").append(status);
+        $("#tableBody > tr:nth-child(5) > td:nth-child(6)").append(outTime);
+        $("#tableBody > tr:nth-child(5) > td:nth-child(7)").append(duration + ":" + "00");
+        $("#tableBody > tr:nth-child(5) > td:nth-child(8)").append(expectedReturnTime);
+        } 
    }
-   if($('#tableBody > tr').hasClass('active') === false){
-    alert("please select a row")
-   }
+   
 }
 
 function staffIn(){
-
+    var status = "In"
+    if($('#tableBody > tr:nth-child(1)').hasClass('active') === true){
+        $("#tableBody > tr:nth-child(1) > td:nth-child(5)").empty().append(status);
+        $("#tableBody > tr:nth-child(1) > td:nth-child(6)").empty();
+        $("#tableBody > tr:nth-child(1) > td:nth-child(7)").empty();
+        $("#tableBody > tr:nth-child(1) > td:nth-child(8)").empty();
+    } 
+    if($('#tableBody > tr:nth-child(2)').hasClass('active') === true){
+        $("#tableBody > tr:nth-child(2) > td:nth-child(5)").empty().append(status);
+        $("#tableBody > tr:nth-child(2) > td:nth-child(6)").empty();
+        $("#tableBody > tr:nth-child(2) > td:nth-child(7)").empty();
+        $("#tableBody > tr:nth-child(2) > td:nth-child(8)").empty();
+    } 
+    if($('#tableBody > tr:nth-child(3)').hasClass('active') === true){
+        $("#tableBody > tr:nth-child(3) > td:nth-child(5)").empty().append(status);
+        $("#tableBody > tr:nth-child(3) > td:nth-child(6)").empty();
+        $("#tableBody > tr:nth-child(3) > td:nth-child(7)").empty();
+        $("#tableBody > tr:nth-child(3) > td:nth-child(8)").empty();
+    } 
+    if($('#tableBody > tr:nth-child(4)').hasClass('active') === true){
+        $("#tableBody > tr:nth-child(4) > td:nth-child(5)").empty().append(status);
+        $("#tableBody > tr:nth-child(4) > td:nth-child(6)").empty();
+        $("#tableBody > tr:nth-child(4) > td:nth-child(7)").empty();
+        $("#tableBody > tr:nth-child(4) > td:nth-child(8)").empty();
+    } 
+    if($('#tableBody > tr:nth-child(5)').hasClass('active') === true){
+        $("#tableBody > tr:nth-child(5) > td:nth-child(5)").empty().append(status);
+        $("#tableBody > tr:nth-child(5) > td:nth-child(6)").empty();
+        $("#tableBody > tr:nth-child(5) > td:nth-child(7)").empty();
+        $("#tableBody > tr:nth-child(5) > td:nth-child(8)").empty();
+    } if($('#tableBody > tr').hasClass('active') === false){
+        alert("please select a row first")
+       }
 }
 
 
